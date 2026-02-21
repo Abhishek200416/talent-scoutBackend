c81d61980a5a9ed1c972f58e6f191fdd37763300
Abhishek200416 <abhishek20040916@gmial.com>
2026-02-21 13:16:32 +0530

    Add full project

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ WSGI entry point for Gunicorn
 Render deployment uses this to start the application
 """
 
-from .server import app
+from server import app
 
 # Expose the WSGI app callable for Gunicorn
 application = app
