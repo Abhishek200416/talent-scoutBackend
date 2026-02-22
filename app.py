186d5f9cbfe5acee15ff2b16566db62bc4ecb8d2
Abhishek200416 <abhishek20040916@gmial.com>
2026-02-22 14:34:43 +0530

    Add full project

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ Root-level WSGI entry point for Gunicorn
 Required when using: gunicorn app:app
 """
 
-from server import app
+from backend.server import app
 
 # These must be present for gunicorn to find the app
 application = app
